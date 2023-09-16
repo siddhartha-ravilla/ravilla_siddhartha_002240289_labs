ebe49ce50ce938e0085f5a8c935ec7fd2f9ded4e
siddhartha-ravilla <ravilla.si@northeastern.edu>
2023-09-15 23:21:16 -0400

    Lab 1 assignment

---
+++ Lab_1/src/model/Product.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Guest User
+ */
+public class Product {
+    
+    private String name;
+    private String desc;
+    private String availnum;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getAvailnum() {
+        return availnum;
+    }
+
+    public void setAvailnum(String availnum) {
+        this.availnum = availnum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+}
